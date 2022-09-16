a9105e0a586150e376a8d44e3298a8c694dac06e
Ezequiel Alvarez <ezequielb.perez@gmail.com>
2022-09-16 10:03:02 -0300

    initial application.properties, News entity and repository added

--- src/main/java/com/alkemy/ong/OngApplication.java
+++ src/main/java/com/alkemy/ong/OngApplication.java
@@ -1,7 +1,13 @@
 package com.alkemy.ong;
 
+import com.alkemy.ong.entity.NewsEntity;
+import com.alkemy.ong.repository.NewsRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class OngApplication {
@@ -9,5 +15,6 @@ public class OngApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OngApplication.class, args);
 	}
+
 
 }
