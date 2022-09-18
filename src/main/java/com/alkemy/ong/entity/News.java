4b315fbb7fac66de287e55e5979fc934eb4981c6
Ezequiel Alvarez <ezequielb.perez@gmail.com>
2022-09-18 17:16:44 -0300

    String id with strategy = uuid and foreign_key as String categoryId

--- src/main/java/com/alkemy/ong/entity/News.java
+++ src/main/java/com/alkemy/ong/entity/News.java
@@ -2,15 +2,16 @@ package com.alkemy.ong.entity;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.SQLDelete;
-import org.hibernate.annotations.UpdateTimestamp;
-import org.hibernate.annotations.Where;
+import org.hibernate.annotations.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.UUID;
 
 @Getter
@@ -22,18 +23,17 @@ import java.util.UUID;
 public class News {
 
     @Id
-    @GeneratedValue
-    @Column(
-            name = "id",
-            updatable = false
-    )
-    private UUID id;
+    @GeneratedValue(generator="system-uuid")
+    @GenericGenerator(name="system-uuid", strategy = "uuid")
+    private String id;
 
     @Column(
             name = "name",
             nullable = false
     )
     private String name;
+
+
 
     @Column(
             name = "content",
@@ -50,15 +50,29 @@ public class News {
 
     @CreationTimestamp
     @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @Column(
+            name = "create_timestamp",
+            updatable = false
+    )
     private LocalDateTime createDateTime;
 
     @UpdateTimestamp
     @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @Column(
+            name = "update_timestamp"
+    )
     private LocalDateTime updateDateTime;
 
     private boolean deleted = Boolean.FALSE;
 
-    @Column(name = "category_id_fk")
-    private UUID categoryId;
+    @OneToOne
+    @JoinColumn(
+            name = "category_id",
+            referencedColumnName = "id",
+            foreignKey = @ForeignKey(
+                    name = "category_id_fk"
+            )
+    )
+    private String categoryId;
 
 }
--- src/main/java/com/alkemy/ong/repository/NewsRepository.java
+++ src/main/java/com/alkemy/ong/repository/NewsRepository.java
@@ -3,10 +3,7 @@ package com.alkemy.ong.repository;
 import com.alkemy.ong.entity.News;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.UUID;
-
 @Repository
-public interface NewsRepository extends JpaRepository<News, UUID> {
+public interface NewsRepository extends JpaRepository<News, String> {
 
 }
