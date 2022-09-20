49ba488b859f2991688efd4f173ebe3f87e419f6
flavioambroggio <flavioam_02@hotmail.com>
2022-09-20 08:48:14 -0300

    create organization controller, dto, mapper and service

---
+++ src/main/java/com/alkemy/ong/service/OrganizationService.java
@@ -0,0 +1,12 @@
+package com.alkemy.ong.service;
+
+
+import com.alkemy.ong.dto.OrganizationDTO;
+
+import java.util.List;
+
+public interface OrganizationService {
+
+    public List<OrganizationDTO> getOrganizationsDTO();
+
+}
