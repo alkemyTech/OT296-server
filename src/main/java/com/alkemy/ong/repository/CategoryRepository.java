dee77b1aeb0e6ce87e885e8c45a6a772c761be2c
gabibosio <97483155+gabibosio@users.noreply.github.com>
2022-09-21 13:17:06 -0300

    change type of id

--- src/main/java/com/alkemy/ong/repository/CategoryRepository.java
+++ src/main/java/com/alkemy/ong/repository/CategoryRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 import java.util.UUID;
 
 @Repository
-public interface CategoryRepository extends JpaRepository<Category, UUID> {
+public interface CategoryRepository extends JpaRepository<Category, String> {
 }
