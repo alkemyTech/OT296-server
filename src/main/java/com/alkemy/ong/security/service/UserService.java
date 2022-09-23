e9fa240583ef584ed6ab9593710a248f8336b2c5
Ezequiel Alvarez <ezequielb.perez@gmail.com>
2022-09-23 16:40:19 -0300

    spaces in UserService fixed

--- src/main/java/com/alkemy/ong/security/service/UserService.java
+++ src/main/java/com/alkemy/ong/security/service/UserService.java
@@ -3,13 +3,11 @@ package com.alkemy.ong.security.service;
 import javassist.NotFoundException;
 
 import com.alkemy.ong.security.dto.RegisterDTO;
-
 import java.util.Map;
 
 public interface UserService {
     void delete(String id) throws NotFoundException;
 
     RegisterDTO create (RegisterDTO user);
-
     void patchUser(String id, Map<Object, Object> objectMap) throws NotFoundException;
 }
