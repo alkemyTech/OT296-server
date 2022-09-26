06c5d8a8d7515bc1a23e842290bad5502465993c
flavioambroggio <flavioam_02@hotmail.com>
2022-09-26 12:53:05 -0300

    create seed data for activity

--- src/main/java/com/alkemy/ong/seeder/ActivitySeeder.java
+++ src/main/java/com/alkemy/ong/seeder/ActivitySeeder.java
@@ -27,15 +27,15 @@ public class ActivitySeeder implements CommandLineRunner {
         List<Activity> activityList = new ArrayList<>();
 
         Activity activity1 = new Activity();
-        activity1.setName("nombre activity1");
-        activity1.setContent("content activity1");
-        activity1.setImage("images/activity1.png");
+        activity1.setName("Consultoría Ambiental y RSE (Asesoramiento a medida)");
+        activity1.setContent("Nuestro objetivo radica en colaborar con la construcción de un perfil de empresa sustentable que comience desde acciones propias. Promover un cambio de hábitos genuino a fin de poder consolidar la transición hacia un nuevo paradigma, acorde a la ética que demanda la época y a cumplir con los valores de conciencia y responsabilidad del sector privado para con el ambiente.");
+        activity1.setImage("imagen/activity1");
         activityList.add(activity1);
 
         Activity activity2 = new Activity();
-        activity2.setName("nombre activity2");
-        activity2.setContent("content activity2");
-        activity2.setImage("images/activity2.png");
+        activity2.setName("Taller de Huerta en Casa");
+        activity2.setContent("La idea principal de este taller es introducirlos en el mundo de las hortalizas y brindar los conocimientos necesarios para realizar una producción agroecológica en el espacio que se disponga en casa: balcón, patio o terraza.");
+        activity2.setImage("imagen/activity2");
         activityList.add(activity2);
 
         activityRepository.saveAll(activityList);
