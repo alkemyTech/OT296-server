69314a097540a5fca8657f22aaf8e389d577b3eb
gabibosio <97483155+gabibosio@users.noreply.github.com>
2022-09-26 18:08:48 -0300

    OT296-41 method Get Categories

---
+++ src/main/java/com/alkemy/ong/controller/CategoryController.java
@@ -0,0 +1,27 @@
+package com.alkemy.ong.controller;
+
+import com.alkemy.ong.dto.CategoryBasicDTO;
+import com.alkemy.ong.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/categories")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping()
+    public ResponseEntity<List<CategoryBasicDTO>> getCategory(){
+        List<CategoryBasicDTO> categoryBasicDTOS = categoryService.getCategory();
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(categoryBasicDTOS);
+    }
+
+}
