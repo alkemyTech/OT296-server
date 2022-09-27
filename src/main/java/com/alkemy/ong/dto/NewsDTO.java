40dd8e6fda989f00ebc2aa05743ba5c599f97ffd
Brian Nieto <nietobrian435@gmail.com>
2022-09-27 18:00:49 -0300

    see news details

--- src/main/java/com/alkemy/ong/dto/NewsDTO.java
+++ src/main/java/com/alkemy/ong/dto/NewsDTO.java
@@ -6,14 +6,23 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class NewsDTO {
-    private String id;
+
+    @NotNull
+    @NotBlank
     private String name;
+    @NotNull
+    @NotBlank
     private String content;
+    @NotNull
+    @NotBlank
     private String image;
-    private LocalDateTime createDateTime;
     private LocalDateTime updateDateTime;
+
 }
