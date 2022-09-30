c7488f8b971055eea94fa6fe1b992d8633a9a583
FelipeMunevar <felipemunevarn@gmail.com>
2022-09-30 08:24:10 -0300

    adding controller with POST to create testimonials

---
+++ src/main/java/com/alkemy/ong/controller/TestimonialController.java
@@ -0,0 +1,28 @@
+package com.alkemy.ong.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alkemy.ong.dto.TestimonialDTO;
+import com.alkemy.ong.service.TestimonialService;
+
+@RestController
+@RequestMapping("/testimonials")
+public class TestimonialController {
+
+	@Autowired
+	private TestimonialService testimonialService;
+
+	@PostMapping
+	public ResponseEntity<TestimonialDTO> createTestimonial(@Valid @RequestBody TestimonialDTO testimonial) {
+		testimonialService.createTestimonial(testimonial);
+		return ResponseEntity.status(HttpStatus.CREATED).build();
+	}
+}
