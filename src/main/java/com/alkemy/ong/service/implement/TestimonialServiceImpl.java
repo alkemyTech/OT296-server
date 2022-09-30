9e08d884283b9f1775c64706e249be8177f88098
FelipeMunevar <felipemunevarn@gmail.com>
2022-09-30 10:21:05 -0300

    fixin conflicts

--- src/main/java/com/alkemy/ong/service/implement/TestimonialServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/implement/TestimonialServiceImpl.java
@@ -1,20 +1,15 @@
 package com.alkemy.ong.service.implement;
 
-import org.springframework.stereotype.Service;
-
 import com.alkemy.ong.dto.TestimonialDTO;
 import com.alkemy.ong.service.TestimonialService;
+import com.alkemy.ong.entity.Testimonial;
+import com.alkemy.ong.mapper.TestimonialMapper;
+import com.alkemy.ong.repository.TestimonialRepository;
 
 import javassist.NotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.alkemy.ong.dto.TestimonialDTO;
-import com.alkemy.ong.entity.Testimonial;
-import com.alkemy.ong.mapper.TestimonialMapper;
-import com.alkemy.ong.repository.TestimonialRepository;
-import com.alkemy.ong.service.TestimonialService;
 
 @Service
 public class TestimonialServiceImpl implements TestimonialService {
@@ -31,10 +26,17 @@ public class TestimonialServiceImpl implements TestimonialService {
 		Testimonial testimonialSave = testimonialRepository.save(testimonial);
 		return testimonialMapper.testimonialEntity2DTO(testimonialSave);
 	}
-	
+
 	@Override
-	public TestimonialDTO updateTestimonial(TestimonialDTO dto, String id) throws NotFoundException {
-		// TODO Auto-generated method stub
-		return null;
+	public TestimonialDTO updateTestimonial(TestimonialDTO testimonialDTO, String id) throws NotFoundException {
+		Testimonial testimonial= testimonialRepository.findById(id).orElse(null);
+		if (testimonial == null){
+			throw new NotFoundException("testimonial not found");
+		}
+		testimonial.setContent(testimonialDTO.getContent());
+		testimonial.setImage(testimonialDTO.getImage());
+		testimonial.setName(testimonialDTO.getName());
+		Testimonial testimonialUpdated = testimonialRepository.save(testimonial);
+		return testimonialMapper.testimonialEntity2DTO(testimonialUpdated);
 	}
 }
