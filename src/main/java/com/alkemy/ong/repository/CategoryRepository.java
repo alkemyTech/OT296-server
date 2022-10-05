b0d6bed051cbd7f69690660244726f42aa9f3b4e
JoelTejerina <catrieltejerina@gmail.com>
2022-10-05 16:51:12 -0300

    Add in categoryRepository findAll

--- src/main/java/com/alkemy/ong/repository/CategoryRepository.java
+++ src/main/java/com/alkemy/ong/repository/CategoryRepository.java
@@ -1,6 +1,8 @@
 package com.alkemy.ong.repository;
 
 import com.alkemy.ong.entity.Category;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import java.util.UUID;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, String> {
+    Page<Category> findAll(Pageable pageable);
 }
