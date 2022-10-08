30f4cd9022dd284d52a6256995b53e34e8e0907e
FelipeMunevar <felipemunevarn@gmail.com>
2022-10-07 21:41:28 -0300

    adding mapper to GET pageable for testimonials

--- src/main/java/com/alkemy/ong/mapper/TestimonialMapper.java
+++ src/main/java/com/alkemy/ong/mapper/TestimonialMapper.java
@@ -1,5 +1,8 @@
 package com.alkemy.ong.mapper;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Component;
 
 import com.alkemy.ong.dto.TestimonialDTO;
@@ -22,5 +25,13 @@ public class TestimonialMapper {
 		testimonialDTO.setImage(testimonial.getImage());
 		testimonialDTO.setName(testimonial.getName());
 		return testimonialDTO; 
+	}
+	
+	public List<TestimonialDTO> testimonialEntityPageDTOList(List<Testimonial> testimonials){
+		List<TestimonialDTO> testimonialDTO = new ArrayList<>();
+		for(Testimonial testimonial: testimonials){
+			testimonialDTO.add(this.testimonialEntity2DTO(testimonial));
+		}
+		return testimonialDTO;
 	}
 }
