8f88b6252af7f77f74d7bab1add1993a10cd63e4
FelipeMunevar <felipemunevarn@gmail.com>
2022-10-19 10:36:11 -0300

    fixing mock of loginDTO

--- src/test/java/com/alkemy/ong/util/LoginMocksUtil.java
+++ src/test/java/com/alkemy/ong/util/LoginMocksUtil.java
@@ -3,28 +3,20 @@ package com.alkemy.ong.util;
 import com.alkemy.ong.security.dto.LoginDTO;
 
 public class LoginMocksUtil {
-	
-    public static LoginDTO generateExistentLoginDTO(){
-        LoginDTO loginDTO = new LoginDTO();
-        loginDTO.setEmail("felimune@dev.com");
-        loginDTO.setPassword("pass1234");
 
-        return loginDTO;
-    }
-    
-    public static LoginDTO generateFakeLoginDTO(){
-        LoginDTO loginDTO = new LoginDTO();
-        loginDTO.setEmail("pepe@botellas.com");
-        loginDTO.setPassword("A123");
+	public static LoginDTO generateExistentLoginDTO(){
+		LoginDTO loginDTO = new LoginDTO();
+		loginDTO.setEmail("felimune@dev.com");
+		loginDTO.setPassword("pass1234");
 
-        return loginDTO;
-    }
-    
-    public static LoginDTO generateRequestMissingMandatoryAttributes(){
-        LoginDTO loginDTO = new LoginDTO();
-        loginDTO.setPassword("userMock123");
+		return loginDTO;
+	}
 
-        return loginDTO;
-    }
+	public static LoginDTO generateFakeLoginDTO(){
+		LoginDTO loginDTO = new LoginDTO();
+		loginDTO.setEmail("pepe@botellas.com");
+		loginDTO.setPassword("A123");
 
+		return loginDTO;
+	}
 }
