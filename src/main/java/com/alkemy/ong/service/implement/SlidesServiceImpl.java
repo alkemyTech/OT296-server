52135bd59b3b55e62386064b03c58e92247ed869
macarogo <mariacarolinagomez88@gmail.com>
2022-10-22 19:57:42 -0300

    OT296-101 Slides Service Test

--- src/main/java/com/alkemy/ong/service/implement/SlidesServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/implement/SlidesServiceImpl.java
@@ -9,13 +9,14 @@ import com.alkemy.ong.repository.OrganizationRepository;
 import com.alkemy.ong.repository.SlidesRepository;
 import com.alkemy.ong.service.SlidesService;
 import javassist.NotFoundException;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.io.IOException;
 import java.util.*;
-
+@AllArgsConstructor
 @Service
 public class SlidesServiceImpl implements SlidesService {
     @Autowired
