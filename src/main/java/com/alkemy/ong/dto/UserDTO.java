ced84fb3992a9c462b2d00ac0080ae7d0a6580f3
Ezequiel Alvarez <ezequielb.perez@gmail.com>
2022-10-25 15:31:57 -0300

    minor fixes

--- src/main/java/com/alkemy/ong/dto/UserDTO.java
+++ src/main/java/com/alkemy/ong/dto/UserDTO.java
@@ -2,9 +2,6 @@ package com.alkemy.ong.dto;
 
 import com.alkemy.ong.entity.Role;
 import lombok.Builder;
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 import java.time.LocalDateTime;
 
